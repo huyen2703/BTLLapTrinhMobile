1e5538de94d773ee868888833068a22c18bae821
huyen2703 <nhohuyen2703@gmail.com>
2023-12-21 20:19:31 +0700

    project

---
+++ app/src/main/java/com/example/baitaplonlaptrinhmobile/StartActivity.java
@@ -0,0 +1,31 @@
+package com.example.baitaplonlaptrinhmobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+                if(currentUser==null){
+                    startActivity(new Intent(StartActivity.this,LoginActivity.class));
+                }else{
+                    startActivity(new Intent(StartActivity.this,MainActivity.class));
+                }
+                finish();
+            }
+        },1000);
+    }
+}

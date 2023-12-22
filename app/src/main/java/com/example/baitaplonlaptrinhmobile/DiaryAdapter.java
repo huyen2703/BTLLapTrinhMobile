d7791ccfe465e76c436b75c69fa3e42fec2012f4
huyen2703 <nhohuyen2703@gmail.com>
2023-12-22 20:29:43 +0700

    project

--- app/src/main/java/com/example/baitaplonlaptrinhmobile/DiaryAdapter.java
+++ app/src/main/java/com/example/baitaplonlaptrinhmobile/DiaryAdapter.java
@@ -1,4 +1,47 @@
 package com.example.baitaplonlaptrinhmobile;
 
-public class DiaryAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.api.Context;
+
+public class DiaryAdapter extends FirestoreRecyclerAdapter<Diary, DiaryAdapter.DiaryViewHolder> {
+    Context context;
+    public DiaryAdapter(@NonNull FirestoreRecyclerOptions<Diary> options, Context context) {
+        super(options);
+        this.context = context;
+    }
+
+    @Override
+    protected void onBindViewHolder(@NonNull DiaryViewHolder holder, int position, @NonNull Diary model) {
+        holder.titleTextView.setText(model.title);
+        holder.contentTextView.setText(model.content);
+        holder.timestampTextView.setText(Utility.timestampToString(model.timestamp));
+    }
+
+    @NonNull
+    @Override
+    public DiaryViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycler_diary_item,parent,false);
+        return new DiaryViewHolder(view);
+    }
+
+    class DiaryViewHolder extends RecyclerView.ViewHolder{
+
+
+        TextView titleTextView,contentTextView,timestampTextView;
+        public DiaryViewHolder(@NonNull View itemView) {
+            super(itemView);
+            titleTextView = itemView.findViewById(R.id.diarys_title_text);
+            contentTextView = itemView.findViewById(R.id.diary_content_text_view);
+            timestampTextView = itemView.findViewById(R.id.diarys_title_text);
+        }
+    }
 }
